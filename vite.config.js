063f564854baecf708e6508fc4d9bfda53223d7e
khaidang71 <khaidango0kd@gmail.com>
2022-11-01 09:28:09 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,19 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    },
   },
+
   server: {
     port: 3001,
-    },
-})
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+      },
+
+    }
+
+  },
+
+});
