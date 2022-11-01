d437943f45d58c90e6bcdbf6928c74e04efa94e3
khaidang71 <khaidango0kd@gmail.com>
2022-11-01 15:20:46 +0700

    Them lien he moi

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -37,7 +37,6 @@
           <i class="fas fa-address-card"></i>
         </h4>
         <ContactCard :contact="activeContact" />
-        <ContactCard :contact="activeContact" />
         <router-link
           :to="{
             name: 'contact.edit',
